d3b7ebcccd3e556b06692f643f64abb3ac3f9d37
jnwaletzko <jnwaletzko@gmail.com>
2014-11-05 17:46:54 -0600

    Design patterns

---
+++ Ducks/MiniDuckSimulator/MiniDuckSimulator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ducks;
+
+namespace MiniDuckSimulator
+{
+    public class MiniDuckSimulator
+    {
+        public static void Main(String[] args)
+        {
+            Duck mallard = new MallardDuck();
+            mallard.performQuack();
+            mallard.performFly();
+
+            Duck model = new ModelDuck();
+            model.performFly();
+            model.setFlyBehavior(new FlyRocketPowered());
+            model.performFly();
+        }
+    }
+}
